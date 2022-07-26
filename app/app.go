2ded79612a0a2d7deab2fe745f1833accc75a33b
Etvendy Afrilio <etvendy@gmail.com>
2022-07-26 12:27:20 +0700

    new commit

--- app/app.go
+++ app/app.go
@@ -2,15 +2,18 @@ package app
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
 
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
 
 	// * defining routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getCustomers)
+	mux.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	mux.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	mux.HandleFunc("/customers/{customers_id}", getCustomers).Methods(http.MethodGet)
 
 	// * starting the server
 	http.ListenAndServe(":8080", mux)
--- app/handlers.go
+++ app/handlers.go
@@ -5,24 +5,28 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Customer struct {
+	ID      int    `json:"id" xml:"id"`
 	Name    string `json:"name" xml:"name"`
 	City    string `json:"city" xml:"city"`
 	Zipcode string `json:"zip_code" xml:"zipcode"`
 }
 
 var customers []Customer = []Customer{
-	{"User1", "Jakarta", "12345"},
-	{"User2", "Surabaya", "67890"},
+	{1, "User1", "Jakarta", "12345"},
+	{2, "User2", "Surabaya", "67890"},
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello Celerates!")
 }
 
-func getCustomers(w http.ResponseWriter, r *http.Request) {
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprint(w, "Get Customers Endpoint!")
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Add("Content-Type", "application/xml")
@@ -31,4 +35,24 @@ func getCustomers(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(customers)
 	}
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	// get route variabel
+	vars := mux.Vars(r)
+
+	customerId := vars["customer_id"]
+	//convert string to int
+	id, _ := strconv.Atoi(customerId)
+	//searching customer data
+	var cust Customer
+
+	for _, data := range customers {
+		if data.ID == id {
+			cust = data
+		}
+	}
+	//return customer data
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(cust)
 }
