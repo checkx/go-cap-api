33ebda27160aed47853545ec195d2d68f78336c5
Etvendy Afrilio <etvendy@gmail.com>
2022-07-26 15:05:55 +0700

    new commit

--- app/app.go
+++ app/app.go
@@ -8,12 +8,15 @@ import (
 
 func Start() {
 
+	// * create ServeMux
 	mux := mux.NewRouter()
 
 	// * defining routes
 	mux.HandleFunc("/greet", greet).Methods(http.MethodGet)
 	mux.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
-	mux.HandleFunc("/customers/{customers_id}", getCustomers).Methods(http.MethodGet)
+	mux.HandleFunc("/customers", addCustomer).Methods(http.MethodPost)
+
+	mux.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	// * starting the server
 	http.ListenAndServe(":8080", mux)
--- app/handlers.go
+++ app/handlers.go
@@ -14,7 +14,7 @@ type Customer struct {
 	ID      int    `json:"id" xml:"id"`
 	Name    string `json:"name" xml:"name"`
 	City    string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zipcode"`
+	ZipCode string `json:"zip_code" xml:"zipcode"`
 }
 
 var customers []Customer = []Customer{
@@ -27,7 +27,7 @@ func greet(w http.ResponseWriter, r *http.Request) {
 }
 
 func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprint(w, "Get Customers Endpoint!")
+	// fmt.Fprint(w, "get customer endpoint\n")
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Add("Content-Type", "application/xml")
 		xml.NewEncoder(w).Encode(customers)
@@ -37,14 +37,22 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getCustomers(w http.ResponseWriter, r *http.Request) {
-	// get route variabel
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+
+	// * get route variable
 	vars := mux.Vars(r)
 
 	customerId := vars["customer_id"]
-	//convert string to int
-	id, _ := strconv.Atoi(customerId)
-	//searching customer data
+
+	// * convert string to int
+	id, err := strconv.Atoi(customerId)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "invalid customer id")
+		return
+	}
+
+	// * searching customer data
 	var cust Customer
 
 	for _, data := range customers {
@@ -52,7 +60,38 @@ func getCustomers(w http.ResponseWriter, r *http.Request) {
 			cust = data
 		}
 	}
-	//return customer data
+
+	if cust.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "customer data not found")
+		return
+	}
+
+	// * return customer data
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(cust)
+}
+
+func addCustomer(w http.ResponseWriter, r *http.Request) {
+	// * decode request body
+	var cust Customer
+	json.NewDecoder(r.Body).Decode(&cust)
+
+	// * generate new id
+	nextID := getNextID()
+	cust.ID = nextID
+
+	// * save data to array
+	customers = append(customers, cust)
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintln(w, "customer successfully created")
+}
+
+func getNextID() int {
+	lastIndex := len(customers) - 1
+	lastCustomer := customers[lastIndex]
+	// cust := customers[len(customers)-1]
+
+	return lastCustomer.ID + 1
 }
